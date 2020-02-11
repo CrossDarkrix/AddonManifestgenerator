e3e8ac7be6094f50961e519dc4d9f6de9f6353b4
CrossDarkrix <60906986+CrossDarkrix@users.noreply.github.com>
2020-02-11 11:27:05 +0900

    master
    最初の追加

---
+++ ManifestGenerator[ENG].py
@@ -0,0 +1,179 @@
+﻿# -*- coding: utf-8 -*-
+
+import json
+import uuid
+import sys
+
+#User Input
+print("◆◆◆ Minecraft Addon Manifest Generator v1.5.0 ◆◆◆")
+pack_name = input("Pack Name: ")
+descript = input("Pack Description: ")
+pack_type = input("Pack Type(resources(ResourcePack) or data(behava pack) or skins(Skin Pack))?: ").replace('Skins','skins').replace('Data','data').replace('DATA','data').replace('Resources','resources').replace('Respack','resources').replace('Sinks','skins').replace('SkinPack','skins').replace('behava pack','data').replace('ResourcePack','resources').replace('Skin Pack','skins').replace('skin','skins').replace('behavapack','data')
+if pack_type == '':
+	print('Error: "Pack Type" is Empty!')
+	sys.exit(1)
+else:
+	pass
+
+def behava_pack():
+	pack_ver = input('Pack Version: ').replace('.',',').replace(' ',',')
+	min_engines = input('min_engine Version(Default: 1.11.0): ').replace('.',',').replace(' ',',')
+	if min_engines == '':
+		min_engines = ('1.11.0').replace('.',',').replace(' ',',')
+	else:
+		pass
+
+	#Json Template
+	raw_manifest = ("""{
+	   "format_version" : 1,
+	   "header" : {
+		  "description" : "",
+		  "name" : "",
+		  "uuid" : "",
+		  "min_engine_version": [MIN_VERSION],
+		  "version" : [PackVersion]
+	   },
+	   "modules" : [
+		  {
+			 "description" : "",
+			 "type" : "",
+			 "uuid" : "",
+			 "version" : [PackVersion]
+		  }
+	   ]
+	}
+	""").replace('PackVersion', pack_ver).replace('MIN_VERSION', min_engines)
+
+	#Reading JsonData
+	load_manifest = json.loads(raw_manifest)
+
+	#Generate UUID4
+	uuid_1 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+	uuid_2 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+
+	#Replace "header"/
+	load_manifest['header']['description'] = descript
+	load_manifest['header']['name'] = pack_name
+	load_manifest['header']['uuid'] = uuid_1
+
+	#replace "modules"/
+	load_manifest['modules'][0]['description'] = descript
+	load_manifest['modules'][0]['type'] = 'data'
+	load_manifest['modules'][0]['uuid'] = uuid_2
+
+	JSON_Data = json.dumps(load_manifest, indent=2, ensure_ascii=False)
+
+	#writing manifest.json
+	f = open('manifest.json','w', encoding='utf-8')
+	f.write(JSON_Data)
+	f.close()
+
+
+def skin_pack():
+	pack_ver = input('Pack Version: ').replace('.',',').replace(' ',',')
+	#Json Template
+	raw_manifest = ("""{
+	   "format_version" : 1,
+	   "header" : {
+		  "description" : "",
+		  "name" : "",
+		  "uuid" : "",
+		  "version" : [PackVersion]
+	   },
+	   "modules" : [
+		  {
+			 "description" : "",
+			 "type" : "",
+			 "uuid" : "",
+			 "version" : [PackVersion]
+		  }
+	   ]
+	}
+	""").replace('PackVersion', pack_ver)
+
+	#Reading JsonData
+	load_manifest = json.loads(raw_manifest)
+
+	#Generate UUID4
+	uuid_1 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+	uuid_2 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+
+	#Replace "header"/
+	load_manifest['header']['description'] = descript
+	load_manifest['header']['name'] = pack_name
+	load_manifest['header']['uuid'] = uuid_1
+
+	#replace "modules"/
+	load_manifest['modules'][0]['description'] = descript
+	load_manifest['modules'][0]['type'] = 'skinpack'
+	load_manifest['modules'][0]['uuid'] = uuid_2
+
+	JSON_Data = json.dumps(load_manifest, indent=2, ensure_ascii=False)
+
+	#writing manifest.json
+	f = open('manifest.json','w', encoding='utf-8')
+	f.write(JSON_Data)
+	f.close()
+
+
+def resources_pack():
+	pack_ver = input('Pack Version: ').replace('.',',').replace(' ',',')
+	#Json Template
+	raw_manifest = ("""{
+	   "format_version" : 1,
+	   "header" : {
+		  "description" : "",
+		  "name" : "",
+		  "uuid" : "",
+		  "version" : [PackVersion]
+	   },
+	   "modules" : [
+		  {
+			 "description" : "",
+			 "type" : "",
+			 "uuid" : "",
+			 "version" : [PackVersion]
+		  }
+	   ]
+	}
+	""").replace('PackVersion', pack_ver)
+
+	#Reading JsonData
+	load_manifest = json.loads(raw_manifest)
+
+	#Generate UUID4
+	uuid_1 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+	uuid_2 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+
+	#Replace "header"/
+	load_manifest['header']['description'] = descript
+	load_manifest['header']['name'] = pack_name
+	load_manifest['header']['uuid'] = uuid_1
+
+	#replace "modules"/
+	load_manifest['modules'][0]['description'] = descript
+	load_manifest['modules'][0]['type'] = 'resources'
+	load_manifest['modules'][0]['uuid'] = uuid_2
+
+	JSON_Data = json.dumps(load_manifest, indent=2, ensure_ascii=False)
+
+	#writing manifest.json
+	f = open('manifest.json','w', encoding='utf-8')
+	f.write(JSON_Data)
+	f.close()
+
+if pack_type == 'data':
+	print("\n>>Selected Behavior Pack!\n")
+	behava_pack()
+else:
+	pass
+if pack_type == 'resources':
+	print("\n>>Selected Resources Pack!\n")
+	resources_pack()
+else:
+	pass
+if pack_type == 'skins':
+	print("\n>>Selected Skin Pack!\n")
+	skin_pack()
+else:
+	pass
---
+++ ManifestGenerator[Ja].py
@@ -0,0 +1,186 @@
+﻿# -*- coding: utf-8 -*-
+
+"""
+テキトーに作った割には上手くいったスクリプト。
+manifest.jsonを手動で書き変えるのは面倒だと感じませんか？
+そんな人の為に作ったスクリプトです。
+"""
+
+import json
+import uuid
+import sys
+
+#ユーザー入力
+print("◆◆◆ Minecraft Addon Manifest Generator v1.5.0 ◆◆◆")
+pack_name = input("Pack名: ")
+descript = input("Packの説明文: ")
+pack_type = input("Packのタイプ(resources(リソースパック) or data(ビヘイビアー) or skins(スキンパック))?: ").replace('Skins','skins').replace('Data','data').replace('DATA','data').replace('Resources','resources').replace('Respack','resources').replace('Sinks','skins').replace('SkinPack','skins').replace('スキン', 'skins').replace('スキンパック', 'skins').replace('リソースパック','resources').replace('ビヘイビアー', 'data').replace('skin','skins')
+if pack_type == '':
+	print('エラー: 入力された文字列が空欄でした')
+	sys.exit(1)
+else:
+	pass
+
+def behava_pack():
+	pack_ver = input('Packのバージョン: ').replace('.',',').replace(' ',',')
+	min_engines = input('min_engineのバージョン(デフォルトの場合はそのままエンターを押してください): ').replace('.',',').replace(' ',',')
+	if min_engines == '':
+		min_engines = ('1.11.0').replace('.',',').replace(' ',',')
+	else:
+		pass
+
+	raw_data_manifest = ("""{
+	   "format_version" : 1,
+	   "header" : {
+		  "description" : "",
+		  "name" : "",
+		  "uuid" : "",
+		  "min_engine_version": [MIN_VERSION],
+		  "version" : [PackVersion]
+	   },
+	   "modules" : [
+		  {
+			 "description" : "",
+			 "type" : "",
+			 "uuid" : "",
+			 "version" : [PackVersion]
+		  }
+	   ]
+	}
+	""").replace('PackVersion', pack_ver).replace('MIN_VERSION', min_engines)
+
+	#JSON形式の読み込み
+	load_manifest = json.loads(raw_data_manifest)
+
+	#ここからUUID
+	uuid_1 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+	uuid_2 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+
+	#ここからJSON内の値変更
+	#"header"部分の値変更
+	load_manifest['header']['description'] = descript
+	load_manifest['header']['name'] = pack_name
+	load_manifest['header']['uuid'] = uuid_1
+
+	#"modules"部分の値変更
+	load_manifest['modules'][0]['description'] = descript
+	load_manifest['modules'][0]['type'] = 'data'
+	load_manifest['modules'][0]['uuid'] = uuid_2
+
+	JSON_Data = json.dumps(load_manifest, indent=2, ensure_ascii=False)
+
+	#manifest.jsonとして書き出し
+	f = open('manifest.json','w', encoding='utf-8')
+	f.write(JSON_Data)
+	f.close()
+
+
+def resources_pack():
+	pack_ver = input('Packのバージョン: ').replace('.',',').replace(' ',',')
+	#Json Template
+	raw_manifest = ("""{
+	   "format_version" : 1,
+	   "header" : {
+		  "description" : "",
+		  "name" : "",
+		  "uuid" : "",
+		  "version" : [PackVersion]
+	   },
+	   "modules" : [
+		  {
+			 "description" : "",
+			 "type" : "",
+			 "uuid" : "",
+			 "version" : [PackVersion]
+		  }
+	   ]
+	}
+	""").replace('PackVersion', pack_ver)
+
+	#JSON形式の読み込み
+	load_manifest = json.loads(raw_manifest)
+
+	#ここからUUID
+	uuid_1 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+	uuid_2 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+
+	#ここからJSON内の値変更
+	#"header"部分の値変更
+	load_manifest['header']['description'] = descript
+	load_manifest['header']['name'] = 'resources'
+	load_manifest['header']['uuid'] = uuid_1
+
+	#"modules"部分の値変更
+	load_manifest['modules'][0]['description'] = descript
+	load_manifest['modules'][0]['type'] = pack_type
+	load_manifest['modules'][0]['uuid'] = uuid_2
+
+	JSON_Data = json.dumps(load_manifest, indent=2, ensure_ascii=False)
+
+	#manifest.jsonとして書き出し
+	f = open('manifest.json','w', encoding='utf-8')
+	f.write(JSON_Data)
+	f.close()
+
+def skins_pack():
+	pack_ver = input('Packのバージョン: ').replace('.',',').replace(' ',',')
+	#Json Template
+	raw_manifest = ("""{
+	   "format_version" : 1,
+	   "header" : {
+		  "description" : "",
+		  "name" : "",
+		  "uuid" : "",
+		  "version" : [PackVersion]
+	   },
+	   "modules" : [
+		  {
+			 "description" : "",
+			 "type" : "",
+			 "uuid" : "",
+			 "version" : [PackVersion]
+		  }
+	   ]
+	}
+	""").replace('PackVersion', pack_ver)
+
+	#JSON形式の読み込み
+	load_manifest = json.loads(raw_manifest)
+
+	#ここからUUID
+	uuid_1 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+	uuid_2 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+
+	#ここからJSON内の値変更
+	#"header"部分の値変更
+	load_manifest['header']['description'] = descript
+	load_manifest['header']['name'] = 'skinpack'
+	load_manifest['header']['uuid'] = uuid_1
+
+	#"modules"部分の値変更
+	load_manifest['modules'][0]['description'] = descript
+	load_manifest['modules'][0]['type'] = 'skinpack'
+	load_manifest['modules'][0]['uuid'] = uuid_2
+
+	JSON_Data = json.dumps(load_manifest, indent=2, ensure_ascii=False)
+
+	#manifest.jsonとして書き出し
+	f = open('manifest.json','w', encoding='utf-8')
+	f.write(JSON_Data)
+	f.close()
+
+if pack_type == 'data':
+	print("\n>>Behaviorパックを選択しました!\n")
+	behava_pack()
+else:
+	pass
+if pack_type == 'resources':
+	print("\n>>Resourcesパックを選択しました!\n")
+	resources_pack()
+else:
+	pass
+if pack_type == 'skins':
+	print("\n>>Skinパックを選択しました!\n")
+	skins_pack()
+else:
+	pass
