e3c7ff9ca69d509e3833c621689898085d13e5fe
CrossDarkrix <60906986+CrossDarkrix@users.noreply.github.com>
2020-04-20 14:57:16 +0900

    Master
    Update

--- ManifestGenerator[ENG].py
+++ ManifestGenerator[ENG].py
@@ -5,13 +5,12 @@ import uuid
 import sys
 
 #User Input
-print("◆◆◆ Minecraft Addon Manifest Generator v1.5.0 ◆◆◆")
+print("◆◆◆ Minecraft Addon Manifest Generator(EN) v1.5.1 ◆◆◆")
 pack_name = input("Pack Name: ")
 descript = input("Pack Description: ")
 pack_type = input("Pack Type(resources(ResourcePack) or data(behava pack) or skins(Skin Pack))?: ").replace('Skins','skins').replace('Data','data').replace('DATA','data').replace('Resources','resources').replace('Respack','resources').replace('Sinks','skins').replace('SkinPack','skins').replace('behava pack','data').replace('ResourcePack','resources').replace('Skin Pack','skins').replace('skin','skins').replace('behavapack','data')
 if pack_type == '':
-	print('Error: "Pack Type" is Empty!')
-	sys.exit(1)
+	sys.exit('Error: "Pack Type" is Empty!')
 else:
 	pass
 
@@ -116,11 +115,11 @@ def skin_pack():
 	f.close()
 
 
-def resources_pack():
+def Resources_pack():
 	pack_ver = input('Pack Version: ').replace('.',',').replace(' ',',')
 	#Json Template
 	raw_manifest = ("""{
-	   "format_version" : 1,
+	   "format_version" : 2,
 	   "header" : {
 		  "description" : "",
 		  "name" : "",
@@ -162,16 +161,78 @@ def resources_pack():
 	f.write(JSON_Data)
 	f.close()
 
+def Data_Resources_pack():
+	pack_ver = input('Pack Version: ').replace('.',',').replace(' ',',')
+	min_engines = input('min_engine Version(Default: 1.11.0): ').replace('.',',').replace(' ',',')
+	if min_engines == '':
+		min_engines = ('1.11.0').replace('.',',').replace(' ',',')
+	else:
+		pass
+	#Json Template
+	raw_manifest = ("""{
+	   "format_version" : 2,
+	   "header" : {
+		  "description" : "",
+		  "name" : "",
+		  "uuid" : "",
+		  "min_engine_version": [MIN_VERSION],
+		  "version" : [PackVersion]
+	   },
+	   "modules" : [
+		  {
+			 "description" : "",
+			 "type" : "",
+			 "uuid" : "",
+			 "version" : [PackVersion]
+		  }
+	   ]
+	}
+	""").replace('PackVersion', pack_ver).replace('MIN_VERSION', min_engines)
+
+	#Reading JsonData
+	load_manifest = json.loads(raw_manifest)
+
+	#Generate UUID4
+	uuid_1 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+	uuid_2 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+
+	#Replace "header"/
+	load_manifest['header']['description'] = descript
+	load_manifest['header']['name'] = pack_name
+	load_manifest['header']['uuid'] = uuid_1
+
+	#replace "modules"/
+	load_manifest['modules'][0]['description'] = descript
+	load_manifest['modules'][0]['type'] = 'resources'
+	load_manifest['modules'][0]['uuid'] = uuid_2
+
+	JSON_Data = json.dumps(load_manifest, indent=2, ensure_ascii=False)
+
+	#writing manifest.json
+	f = open('manifest.json','w', encoding='utf-8')
+	f.write(JSON_Data)
+	f.close()
+
 if pack_type == 'data':
 	print("\n>>Selected Behavior Pack!\n")
 	behava_pack()
 else:
 	pass
 if pack_type == 'resources':
-	print("\n>>Selected Resources Pack!\n")
-	resources_pack()
+	DataR_branch_point = input("DataPack?(1) or Normal?(0): ")
+	if DataR_branch_point == '1':
+		Data_Resources_pack()
+	if DataR_branch_point == '0':
+		Resources_pack()
+	if DataR_branch_point == 'DataPack':
+		Data_Resources_pack()
+	if DataR_branch_point == 'Normal':
+		Resources_pack()
 else:
-	pass
+	if DataR_branch_point == '':
+		sys.exit("Error: Input Empty!")
+	else:
+		pass
 if pack_type == 'skins':
 	print("\n>>Selected Skin Pack!\n")
 	skin_pack()
--- ManifestGenerator[Ja].py
+++ ManifestGenerator[Ja].py
@@ -11,26 +11,25 @@ import uuid
 import sys
 
 #ユーザー入力
-print("◆◆◆ Minecraft Addon Manifest Generator v1.5.0 ◆◆◆")
+print("◆◆◆ Minecraft Addon Manifest Generator(Ja) v1.5.1 ◆◆◆")
 pack_name = input("Pack名: ")
 descript = input("Packの説明文: ")
 pack_type = input("Packのタイプ(resources(リソースパック) or data(ビヘイビアー) or skins(スキンパック))?: ").replace('Skins','skins').replace('Data','data').replace('DATA','data').replace('Resources','resources').replace('Respack','resources').replace('Sinks','skins').replace('SkinPack','skins').replace('スキン', 'skins').replace('スキンパック', 'skins').replace('リソースパック','resources').replace('ビヘイビアー', 'data').replace('skin','skins')
 if pack_type == '':
-	print('エラー: 入力された文字列が空欄でした')
-	sys.exit(1)
+	sys.exit("エラー: 入力された文字列が空欄でした")
 else:
 	pass
-
+	
 def behava_pack():
 	pack_ver = input('Packのバージョン: ').replace('.',',').replace(' ',',')
-	min_engines = input('min_engineのバージョン(デフォルトの場合はそのままエンターを押してください): ').replace('.',',').replace(' ',',')
+	min_engines = input('min_engineのバージョン(デフォルトは1.11.0): ').replace('.',',').replace(' ',',')
 	if min_engines == '':
 		min_engines = ('1.11.0').replace('.',',').replace(' ',',')
 	else:
 		pass
 
 	raw_data_manifest = ("""{
-	   "format_version" : 1,
+	   "format_version" : 2,
 	   "header" : {
 		  "description" : "",
 		  "name" : "",
@@ -75,11 +74,64 @@ def behava_pack():
 	f.close()
 
 
-def resources_pack():
+def Data_Resources_pack():
+	pack_ver = input('Packのバージョン: ').replace('.',',').replace(' ',',')
+	min_engines = input('min_engineのバージョン(デフォルトは1.11.0): ').replace('.',',').replace(' ',',')
+	if min_engines == '':
+		min_engines = ('1.11.0').replace('.',',').replace(' ',',')
+	else:
+		pass
+	#Json Template
+	raw_manifest = ("""{
+	   "format_version" : 2,
+	   "header" : {
+		  "description" : "",
+		  "name" : "",
+		  "uuid" : "",
+		  "min_engine_version": [MIN_VERSION],
+		  "version" : [PackVersion]
+	   },
+	   "modules" : [
+		  {
+			 "description" : "",
+			 "type" : "",
+			 "uuid" : "",
+			 "version" : [PackVersion]
+		  }
+	   ]
+	}
+	""").replace('PackVersion', pack_ver).replace('MIN_VERSION', min_engines)
+
+	#JSON形式の読み込み
+	load_manifest = json.loads(raw_manifest)
+
+	#ここからUUID
+	uuid_1 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+	uuid_2 = str(uuid.uuid4()).replace('(','').replace(')','').replace('UUID','')
+
+	#ここからJSON内の値変更
+	#"header"部分の値変更
+	load_manifest['header']['description'] = descript
+	load_manifest['header']['name'] = pack_name
+	load_manifest['header']['uuid'] = uuid_1
+
+	#"modules"部分の値変更
+	load_manifest['modules'][0]['description'] = descript
+	load_manifest['modules'][0]['type'] = pack_type
+	load_manifest['modules'][0]['uuid'] = uuid_2
+
+	JSON_Data = json.dumps(load_manifest, indent=2, ensure_ascii=False)
+
+	#manifest.jsonとして書き出し
+	f = open('manifest.json','w', encoding='utf-8')
+	f.write(JSON_Data)
+	f.close()
+
+def Resources_pack():
 	pack_ver = input('Packのバージョン: ').replace('.',',').replace(' ',',')
 	#Json Template
 	raw_manifest = ("""{
-	   "format_version" : 1,
+	   "format_version" : 2,
 	   "header" : {
 		  "description" : "",
 		  "name" : "",
@@ -107,7 +159,7 @@ def resources_pack():
 	#ここからJSON内の値変更
 	#"header"部分の値変更
 	load_manifest['header']['description'] = descript
-	load_manifest['header']['name'] = 'resources'
+	load_manifest['header']['name'] = pack_name
 	load_manifest['header']['uuid'] = uuid_1
 
 	#"modules"部分の値変更
@@ -175,10 +227,21 @@ if pack_type == 'data':
 else:
 	pass
 if pack_type == 'resources':
-	print("\n>>Resourcesパックを選択しました!\n")
-	resources_pack()
+	DataR_branch_point = input("データパック(1) or (0)？: ")
+	if DataR_branch_point == '1':
+		Data_Resources_pack()
+	if DataR_branch_point == '0':
+		Resources_pack()
+	if DataR_branch_point == 'データパック':
+		Data_Resources_pack()
+	if DataR_branch_point == 'ノーマル':
+		Resources_pack()
 else:
-	pass
+	if DataR_branch_point == '':
+		sys.exit("エラー: 入力された文字列が空欄でした")
+	else:
+		pass
+
 if pack_type == 'skins':
 	print("\n>>Skinパックを選択しました!\n")
 	skins_pack()
